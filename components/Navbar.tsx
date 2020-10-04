c9a363b99de2cce7fc1b6075b00fdbf656ed2a36
Akash Bhave <akashbbhave@gmail.com>
2020-10-04 10:35:24 -0400

    Move all components out of sub-directories

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ const Navbar = (props) => {
   return (
     <div className="w-full h-14 bg-white flex items-center space-between p-3">
       <div className="h-8 w-8 bg-purple-theme rounded-lg" />
-      <div className="text-black font-bold ml-3">{props.title ?? "My Classroom"}</div>
+      <div className="text-black font-bold ml-3">
+        {props.title ?? "My Classroom"}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
