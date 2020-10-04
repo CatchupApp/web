dd8f6f52bc0ad3927277d7a23fea5da82327f4d4
Akash Bhave <akashbbhave@gmail.com>
2020-10-03 23:33:36 -0400

    Initialize Next.js project

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const HomePage = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>Welcome to Catchup!</h1>
+    </main>
+  </div>
+);
+
+export default HomePage;
